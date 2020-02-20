10ddec3aaee7e581edaf0c7f7b8cd5f563dd1bd1
Christian Michael <cmich004@ucr.edu>
2020-02-20 15:47:31 -0800

    Clarified things in part1

--- part1/vim_practice_1.cpp
+++ part1/vim_practice_1.cpp
@@ -7,14 +7,16 @@ int main() {
 	//command mode instead of insert mode.
 	//
 	//Use the arrow keys to maneuver for now and uncomment the line of code
-	//below. Enter insert mode by pressing "i" and then leave it by pressing
-	// "esc".  
+	//below (cout << ...) Enter insert mode by pressing "i" (See it in the bottom left?)
+	//and then leave it by pressing "esc".  
+	
 	//cout << "Hello, World!" << endl;
 
-	// Once you've uncommented it, save and quit by entering command mode and typing :wq.
+	// Once you've uncommented it, save and quit by typing :wq from command mode.
 	// When you type : in command mode, you can see the command in the
 	// bottom left corner of your screen.
 	// Compile and run it by typing "g++ vim_practice_1.cpp -o my_first_program.out" 
+	// from the terminal.
 	//
 	// run the program by typing "./my_first_program.out"
 	return 0;
